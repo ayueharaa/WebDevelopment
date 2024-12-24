731b0594c7d30ac8429b979124b3e4b523c24ab7
Rafaela Ayumi Uehara <ayumi.uehara@unesp.br>
2024-12-24 09:55:42 -0300

    Add files via upload

---
+++ 2.1 Using Node/index.js
@@ -0,0 +1,16 @@
+// const fs = require ("fs");
+
+// fs.writeFile ("message.txt", "heyyyyy", (err)=> {
+//     if (err) throw err;
+//     console.log('The file has been saved!');
+// });
+
+// fs.readFile ("message.txt","utf8", (err, data)=>{
+//     if (err) throw err;
+//     console.log (data);
+// });
+
+// import superheroes from "superheroes";
+// var name = superheroes.random();
+
+// console.log ('I am ${name}');
